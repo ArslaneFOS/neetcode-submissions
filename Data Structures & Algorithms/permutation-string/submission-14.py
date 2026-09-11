d50fe90b397195ab1f6dfc2ffd7f8ecc1e878a01
Arslane Fares Ould Slimane <arslane.ouldslimane@viacesi.fr>
2026-09-11 20:24:18 +0200

    Add: permutation-string - submission-14

---
+++ Data Structures & Algorithms/permutation-string/submission-14.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        mp1 = {}
+
+        for c in s1:
+            mp1[c] = mp1.get(c, 0) + 1
+
+        mp2 = {}
+        l = 0
+        for r in range(len(s2)):          
+            mp2[s2[r]] = mp2.get(s2[r], 0) + 1
+            if r - l + 1 > len(s1):
+                if mp2[s2[l]] == 1:
+                    mp2.pop(s2[l])
+                else:
+                    mp2[s2[l]] -= 1
+                l += 1
+
+            if mp1 == mp2:
+                return True
+            
+        return False
